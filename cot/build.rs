3b9373840ded91e9f3ab48f97d92ddeb0469eef2
Mateusz Maćkowski <m4tx@m4tx.pl>
2025-04-08 14:01:42 +0100

    feat: use SCSS (#278)
    This should simplify writing the stylesheets, and improve browser
    compatibility. We can also minify the stylesheets now easily, resulting
    in less data transferred to the users.
    
    Fixes #254

--- cot/build.rs
+++ cot/build.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 #[cfg(all(
     feature = "db",
     not(any(feature = "sqlite", feature = "postgres", feature = "mysql"))
@@ -5,5 +7,33 @@
 compile_error!("feature \"db\" requires one of: \"sqlite\", \"postgres\", \"mysql\" to be enabled");
 
 fn main() {
-    // do nothing; this only checks the feature flags
+    build_css();
+}
+
+fn build_css() {
+    const SCSS_FILES: [(&str, &str); 2] = [
+        ("admin/admin.scss", "static/admin/admin.css"),
+        ("error.scss", "templates/css/error.css"),
+    ];
+
+    let options = scss_options();
+
+    for (scss_file, css_file) in SCSS_FILES {
+        let scss_path = format!("scss/{scss_file}");
+
+        println!("cargo::rerun-if-changed={scss_path}");
+
+        let css = grass::from_path(scss_path, &options).expect("failed to compile SCSS");
+
+        let css_path = PathBuf::from(css_file);
+        let css_dir = css_path
+            .parent()
+            .expect("failed to get CSS parent directory");
+        std::fs::create_dir_all(css_dir).expect("failed to create CSS directory");
+        std::fs::write(css_path, css).expect("failed to write CSS");
+    }
+}
+
+fn scss_options() -> grass::Options<'static> {
+    grass::Options::default().style(grass::OutputStyle::Compressed)
 }
